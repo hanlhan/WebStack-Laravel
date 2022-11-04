877ea4c0a675861fa5e021604d772ad4c8b36081
harry <346026240@qq.com>
2022-11-04 17:14:36 +0800

    默认下载网站logo

--- app/Observers/SiteObserver.php
+++ app/Observers/SiteObserver.php
@@ -16,14 +16,31 @@ class SiteObserver
         if (!$site->thumb) {
 
             $thumbName = 'images/' . md5(uniqid()) . '.png';
+            $fp = fopen('uploads/' . $thumbName, 'a');
 
-            $pinyin = new Pinyin();
-            $title = $pinyin->abbr($site->title, PINYIN_KEEP_ENGLISH);
-            $letter = mb_substr(ucfirst($title), 0, 1);
+            $url = $site->url . '/favicon.ico';
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false ); 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 
-            $generater = new InitialAvatar();
-            $thumb = $generater->name($letter)->size(200)->generate();
-            $thumb->save('uploads/' . $thumbName);
+            $output = curl_exec($ch);
+            curl_close($ch);
+
+            if($output){
+                fwrite($fp, $output);
+                fclose($fp);
+            }
+            else {
+                $pinyin = new Pinyin();
+                $title = $pinyin->abbr($site->title, PINYIN_KEEP_ENGLISH);
+                $letter = mb_substr(ucfirst($title), 0, 1);
+                $generater = new InitialAvatar();
+                $thumb = $generater->name($letter)->size(200)->generate();
+                $thumb->save('uploads/' . $thumbName);
+            }
+
 
             $site->thumb = $thumbName;
             $site->save();
